edb181495fc6ea4614a6de0cc56b639c2dbd3687
DESKTOP-TR8L43U\Usman <usmanfazalkareem2@gmail.com>
2025-03-12 13:59:34 +0500

    databse connent

--- src/lib/supabase.ts
+++ src/lib/supabase.ts
@@ -1,6 +1,6 @@
 import { createClient } from "@supabase/supabase-js";
 
-const supabaseUrl = import.meta.env.VITE_SUPABASE_URL || "";
-const supabaseAnonKey = import.meta.env.VITE_SUPABASE_ANON_KEY || "";
+const supabaseUrl = import.meta.env.VITE_SUPABASE_URL || "https://tnspqanvwsddavkwdfye.supabase.co";
+const supabaseAnonKey = import.meta.env.VITE_SUPABASE_ANON_KEY || "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InRuc3BxYW52d3NkZGF2a3dkZnllIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NDE0MDMxODcsImV4cCI6MjA1Njk3OTE4N30.4-32_dCu5E6qMkCI0ETiLH2CQFZfl4NCq7YZTa7WQek";
 
 export const supabase = createClient(supabaseUrl, supabaseAnonKey);
